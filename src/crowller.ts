485d426f8d0da7c0eeb1f02dd3bc1fb78cadc5f3
ruofei <rf.wangchn@gmail.com>
2020-12-20 23:05:42 +0800

    爬取数据对结构设计和存储

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,3 +1,5 @@
+import fs from "fs";
+import path from "path";
 import superagent from "superagent";
 import cheerio from "cheerio";
 
@@ -6,9 +8,19 @@ interface Course {
   count: number;
 }
 
+interface CourseResult {
+  time: number;
+  data: Course[];
+}
+
+interface Content {
+  [propName: number]: Course[];
+}
+
 class Crowller {
   private secret: string = "x3b174jsx";
   private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`;
+  private filePath = path.resolve(__dirname, "../data/course.json");
 
   async getCourseInfo(html: string) {
     const $ = cheerio.load(html);
@@ -31,11 +43,27 @@ class Crowller {
 
   async getRawHtml() {
     const result = await superagent.get(this.url);
-    const courseInfo = this.getCourseInfo(result.text);
+    return result.text;
+  }
+
+  async generateJsonContent(courseInfo: CourseResult) {
+    let fileContent: Content = {};
+    if (fs.existsSync(this.filePath)) {
+      fileContent = JSON.parse(fs.readFileSync(this.filePath, "utf-8"));
+    }
+    fileContent[courseInfo.time] = courseInfo.data;
+    return fileContent;
+  }
+
+  async initSpiderProcess() {
+    const html = await this.getRawHtml();
+    const courseInfo = await this.getCourseInfo(html);
+    const fileContent = await this.generateJsonContent(courseInfo);
+    fs.writeFileSync(this.filePath, JSON.stringify(fileContent));
   }
 
   constructor() {
-    this.getRawHtml();
+    this.initSpiderProcess();
   }
 }
 
